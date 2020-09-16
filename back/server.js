52daa066e65e63e5af879af06db41362644c881c
juliopurschel <Julio.purschel@gmail.com>
2020-09-16 18:19:08 -0300

    Começando de novo

--- back/server.js
+++ back/server.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const mysql = require('mysql');
-const requireDir = require('require-dir')
-//excuta função do express
+
 const app = express();
 
-//inicando o DB
-const dbCon = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "database"
-});
+app.get('/', (req,res) => {
+    res.send('Hellow')
+} )
 
-dbCon.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected");
-})
-
-requireDir('./src/models');
-
-//quando usuário acessar a rota
-//req é a requisição feita ao servidor como params
-//res é a resposta da requisção
-app.get('/', (req, res ) => {
-   
-
-})
-//ouvir porta 3001 no navegador
 app.listen(3001);
